aa275f6fb58a0f322dd174a5f15dfd41711bbc11
Lehlogonolo Masubelele <l.masubelele@gmail.com>
2022-03-23 14:16:35 +0200

    update

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # nodejs_postgres
+
+## restore db
+pg_restore -d movie /db/movie.dump
+
+## make db dump
+pg_dump -Fc movie > movie.dump
--- postgres/setup-db.sh
+++ postgres/setup-db.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# pg_restore is a utility for restoring a PostgreSQL database from an archive created by pg_dump.
-# Here we are restoring movie.dump into the movie database.
-pg_restore -d movie /db/movie.dump
-echo "Movie database restored!"
+# Import title.basics.tsv
+echo "Importing title.basics.tsv:"
+psql -U postgres -d movie -f /db/import.sql
+echo "title.basics.tsv imported!"
