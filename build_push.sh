077a7eef496d3331f11564a010fe93027c813dea
EC2 Default User <ec2-user@ip-17-0-1-172.ec2.internal>
2023-03-17 12:59:30 +0000

    update

---
+++ build_push.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "Building images..."
+docker-compose build
+sleep 2
+echo "Pushing images..."
+docker push codesenju/pgmoviedb.api.local:latest
+docker push codesenju/nodeapi.api.local:latest
+docker push codesenju/index.api.local:v1
+docker push codesenju/index.api.local:v2
+docker push codesenju/index.api.local:v2
+docker push codesenju/frontend.api.local:latest
+docker push codesenju/index-backend.api.local:latest
+echo "Done!"
