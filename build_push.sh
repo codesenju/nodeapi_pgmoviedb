a4742425dc90272e86544a838d17c14bdfee91a2
Lehlogonolo <codesenju@gmail.com>
2023-04-28 00:26:13 +0200

    clean updates

--- build_push.sh
+++ build_push.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
-echo "Building images..."
-docker-compose build
-sleep 2
-echo "Pushing images..."
-docker push codesenju/pgmoviedb.api.local:latest
-docker push codesenju/nodeapi.api.local:latest
-docker push codesenju/index.api.local:v1
-docker push codesenju/index.api.local:v2
-docker push codesenju/index.api.local:v3
-docker push codesenju/frontend.api.local:latest
-docker push codesenju/index-backend.api.local:latest
-echo "Done!"
+
+cd postgres
+docker buildx build --push --platform linux/arm64/v8,linux/amd64 --tag codesenju/pgmoviedb.api.local:latest .
+
+cd ../nodeapi
+docker buildx build --push --platform linux/arm64/v8,linux/amd64 --tag codesenju/nodeapi.api.local:latest .
+
+cd ../index.api.local
+docker buildx build --push --platform linux/arm64/v8,linux/amd64 --tag codesenju/index.api.local:v1 .
+
+cd ../indexv2.api.local
+docker buildx build --push --platform linux/arm64/v8,linux/amd64 --tag codesenju/index.api.local:v2 .
+
+cd ../indexv3.api.local
+docker buildx build --push --platform linux/arm64/v8,linux/amd64 --tag codesenju/index.api.local:v3 .
+
+cd ../frontend.api.local
+docker buildx build --push --platform linux/arm64/v8,linux/amd64 --tag codesenju/frontend.api.local:latest .
+
+cd ../index-backend.api.local
+docker buildx build --push --platform linux/arm64/v8,linux/amd64 --tag codesenju/index-backend.api.local:latest .
